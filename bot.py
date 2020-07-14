60e6e7eb25c4596a7f52d1ef1fc011dec336c79c
CaptainDrak <ccrinbama@gmail.com>
2020-07-13 22:56:26 -0500

    add advantage but it works this time

--- bot.py
+++ bot.py
@@ -23,48 +23,74 @@ def roll_dice(nsides, ndice):
 def multi_type_roll(user_input):
     rolls=user_input.replace(' ','').split('and')
     sumdice=0
+    messages=[]
     for i in range(len(rolls)):
         ndice,nsides = rolls[i].split('d')
         dice = roll_dice(nsides, ndice)
         sumdice += sum([int(i) for i in dice])
-        #await ctx.send("{}: {}".format(rolls[i],', '.join(dice)))
-    #await ctx.send("Total = {}".format(sumdice))
-    return sumdice
+        print(f"{rolls[i]}: {', '.join(dice)}")
+        messages.append(f"{rolls[i]}: {', '.join(dice)}")
+        print(f"Message to be returned to roll request: {messages}")
+    print(f"Total = {sumdice}")
+    messages.append(f"Total = {sumdice}")
+    print(f"Message to be returned to roll request: {messages}")
+    return sumdice, messages
 
 def single_type_roll(user_input):
+    print(user_input)
+    messages=[]
     ndice,nsides = user_input.split('d')
     dice = roll_dice(nsides, ndice)
     sumdice = sum([int(i) for i in dice])
     if len(dice) > 1:
-        await ctx.send("{} \nTotal = {}".format(', '.join(dice),sumdice))
+        print(f"{', '.join(dice)} \nTotal = {sumdice}")
+        messages.append(f"{', '.join(dice)} \nTotal = {sumdice}")
     else:
-        await ctx.send(dice[0])
-    return sumdice
+        print(f"Total = {dice[0]}")
+        messages.append(f"Total = {dice[0]}")
+    return sumdice, messages
 
 @bot.command(name='roll', help="Dice are being thrown!\nUse format <numberofdice>d<numberofsides>. EX: !roll 2d6 rolls two six sided dice.\nFor multiple dice, separate them with the word 'and'. EX: !roll 2d6 and 3d4 rolls two six sided dice and three four sided dice.")
 async def roll(ctx, *user_input):
-    user_input = ''.join(user_input)
+    print(f"Initial, raw values being passed in: {user_input}")
+    user_input = ' '.join(user_input)
+    print(f"Actual user input: {user_input}")
     advantage = 'with advantage' in user_input
-    user_input.replace('with advantage', '')
+    user_input = user_input.replace('with advantage', '')
+    print(f"User input stripped of potential advantage indicator: {user_input}")
+    if advantage is True:
+        print('advantage!')
+    else:
+        print('no advantage!')
     if 'and' in user_input:
         if advantage is True:
             sums = []
             for _ in range(2):
-                sums.append(multi_type_roll(user_input))
+                this_roll = multi_type_roll(user_input)
+                sums.append(this_roll[0])
+                for i in range(len(this_roll[1])):
+                    await ctx.send(this_roll[1][i])
             await ctx.send('Total with advantage = {}'.format(max(sums)))
         else:
-            multi_type_roll(user_input)
+            this_roll = multi_type_roll(user_input)
+            for i in range(len(this_roll[1])):
+                await ctx.send(this_roll[1][i])
 
     else:
         if advantage is True:
             sums = []
             for _ in range(2):
-                sums.append(single_type_roll(user_input))
-            await ctx.send("Total with advantage = {}".format(max(sums)))
+                this_roll = single_type_roll(user_input)
+                sums.append(this_roll[0])
+                for i in range(len(this_roll[1])):
+                    await ctx.send(this_roll[1][i])
+            await ctx.send(f'Total with advantage = {max(sums)}')
         else:
-            single_type_roll(user_input)
+            this_roll = single_type_roll(user_input)
+            for i in range(len(this_roll[1])):
+                await ctx.send(this_roll[1][i])
 
-@bot.command(name='Ping', help='ping pong bb you know what it is')
+@bot.command(name='ping', help='ping pong bb you know what it is')
 async def ping(ctx):
     await ctx.send('Pong')
 
